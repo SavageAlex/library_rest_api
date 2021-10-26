9ebbf952624e9ed4ef760a8ef32d37fec4a72306
SavageAlex <atalaver@gmail.com>
2021-10-26 21:13:14 +0200

    Edited db fields

--- api/models.py
+++ api/models.py
@@ -1,11 +1,15 @@
+import functools
 from django.db import models
 
 class Book(models.Model):
+    id = models.CharField(primary_key=True, max_length=64)
     title = models.CharField(max_length=256)
-    author = models.CharField(max_length=256)
-    publisher = models.CharField(max_length=256)
-    published_date = models.CharField(max_length=256)
-    description = models.CharField(max_length=256)
+    authors = models.TextField()
+    published_date = models.DateField()
+    categories = models.TextField()
+    avaerage_rating = models.FloatField(null=True, blank=True)
+    ratings_count = models.IntegerField(null=True, blank=True)
+    thumbnail = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.title
--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Book
-        fields = ('title', 'author', 'publisher', 'published_date', 'description')
+        fields = ('id', 'title', 'authors', 'published_date', 'categories', 'avaerage_rating', 'ratings_count', 'thumbnail')
