04ac36b3b720664e2ba19ee9890c1f2688de28f2
SavageAlex <atalaver@gmail.com>
2021-11-01 06:13:39 +0100

    settings file correction

--- stx_django_rest_api/settings.py
+++ stx_django_rest_api/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'stx_django_rest_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
